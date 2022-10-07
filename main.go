65b3fd1fe25ed41856543480102d197e38e29851
hieunguyenduc99 <hieu.nguyen.duc@sobanhang.com>
2022-10-07 16:11:40 +0700

    FEAT: ping pong redis

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package learn_redis
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	fmt.Println("Go Redis Tutorial")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping(context.Background()).Result()
+	fmt.Println(pong, err)
+
+}
